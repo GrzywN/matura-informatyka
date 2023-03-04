b9f8fda72c50fb377181fd2d0558037096c65c04
Karol Binkowski <karolbinkowski3@proton.me>
2023-03-04 14:43:37 +0100

    algo: czy_pierwsza()

---
+++ algorytmy/00-czy-pierwsza.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+
+def czy_pierwsza(liczba: int) -> bool:
+    if liczba < 2:
+        return False
+
+    i = 2
+    while i <= sqrt(liczba):
+        if liczba % i == 0:
+            return False
+        i += 1
+
+    return True
+
+
+# Złożoność czasowa - O(sqrt(n))
+# Złożoność pamięciowa - O(1)
+
+print(czy_pierwsza(2))
+print(czy_pierwsza(5))
+print(czy_pierwsza(12))
+print(czy_pierwsza(1))
+print(czy_pierwsza(8))
+print(czy_pierwsza(25))
+
+# True
+# True
+# False
+# False
+# False
+# False
