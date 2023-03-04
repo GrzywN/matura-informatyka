fe2308366912ed6c067a11a8489af6f09f980370
Karol Binkowski <karolbinkowski3@proton.me>
2023-03-04 18:43:35 +0100

    algo: simple_sort()

---
+++ algorytmy/04-simple-sort.py
@@ -0,0 +1,28 @@
+def simple_sort(tab: list[int], n: int) -> None:
+    for i in range(n):
+        for j in range(n):
+            if tab[i] < tab[j]:
+                tmp = tab[i]
+                tab[i] = tab[j]
+                tab[j] = tmp
+
+
+# Złożoność czasowa - O(n^2)
+# Złożoność pamięciowa - O(1)
+
+tablica = [1, 5, 3, 2, 9]
+n = len(tablica)
+
+print(tablica)
+simple_sort(tablica, 5)
+print(tablica)
+
+# Banalnie prosty niewydajny algorytm sortowania w sam raz na maturkę do części teoretycznej.
+# https://youtu.be/_W0yUJlscRA
+
+# dla i = 1..n
+#   dla j = 1..n
+#     jeżeli tab[i] < tab[j] wykonaj:
+#       tmp <- tab[i]
+#       tab[i] <- tab[j]
+#       tab[j] <- tmp
